40d4d308c82fffe1aea81c9458cb5509be44751e
kalvinho11 <kalvinho11@gmail.com>
2023-09-08 15:53:14 +0200

    Initial commit

---
+++ src/main/java/com/example/inditexttest/InditexttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.inditexttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inditexttest/InditexttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inditexttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
